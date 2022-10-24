646b507406bce0539b25ff467a7c37f397065742
gopal-iosdev <gopal.iosdev@gmail.com>
2022-10-23 22:13:16 -0400

    Finished adding Sandwiches app

--- From Apple/Sandwiches/Sandwiches/ContentView.swift
+++ From Apple/Sandwiches/Sandwiches/ContentView.swift
@@ -8,19 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var store: SandwichStore
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List() {
+                ForEach(store.sandwiches) { sandwich in
+                    HStack {
+                        SandwichCell(sandwich: sandwich)
+                    }
+                }
+                .onMove(perform: moveSandwiches)
+                .onDelete(perform: deleteSandwiches)
+                
+                HStack {
+                    Spacer()
+                    Text("\(store.sandwiches.count) Sandwiches")
+                        .foregroundColor(.secondary)
+                    Spacer()
+                }
+            }
+            .navigationTitle("Sandwiches")
+            .toolbar {
+                #if os(iOS)
+                ToolbarItem(placement: ToolbarItemPlacement.navigationBarLeading) {
+                    EditButton()
+                }
+                #endif
+                
+                ToolbarItem(placement: ToolbarItemPlacement.navigationBarTrailing) {
+                    Button("Add", action: makeSandwich)
+                }
+            }
+            
+            Text("Select a sandwich")
+                .font(.largeTitle)
         }
-        .padding()
+    }
+    
+    func makeSandwich() {
+        withAnimation {
+            store.sandwiches.append(Sandwich(name: "Patty Melt",
+                                             ingredientCount: 3))
+        }
+    }
+
+    func moveSandwiches(from: IndexSet, to: Int) {
+        withAnimation {
+            store.sandwiches.move(fromOffsets: from, toOffset: to)
+        }
+    }
+
+    func deleteSandwiches(offsets: IndexSet) {
+        withAnimation {
+            store.sandwiches.remove(atOffsets: offsets)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(store: testStore)
+        ContentView(store: testStore)
+    }
+}
+
+struct SandwichCell: View {
+    var sandwich: Sandwich
+    
+    var body: some View {
+        NavigationLink(destination: SandwichDetail(sandwich: sandwich)) {
+            Image(sandwich.thumbnailName)
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading) {
+                Text(sandwich.name)
+                Text("\(sandwich.ingredientCount) ingredients")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
     }
 }
